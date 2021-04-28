9eff3c278fd45bb86deae0fcfaf5135f7751bb33
Brian Sturgis <sturujisu@gmail.com>
2021-04-28 13:08:58 -0700

    update project with a zero cap on kegs so thet wont go negative

--- src/components/KegDetail.js
+++ src/components/KegDetail.js
@@ -10,7 +10,12 @@ function KegDetail(props){
       <h3>price per keg: {keg.price}</h3>
       <h3>Alcohol Content: {keg.alcoholContent}</h3>
       <h3>Pints Remaining: {keg.pintsLeft}</h3>
+      {keg.pintsLeft > 0 &&
       <button onClick={props.onClickingBuy} className="btn btn-success">Buy Pint</button>
+      }
+      {keg.pintsLeft === 0 &&
+      <h1>sorry no more beers</h1>
+      }
       <button onClick={props.onClickingRestock} className="btn btn-info">refill keg</button>
       <button onClick={ ()=>onClickingEdit (keg.id)}>Update Keg details</button>
       <button onClick={()=> onClickingDelete(keg.id)}>Close out Keg</button>
